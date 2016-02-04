0908af923a6f2d1b9018b18fa9202d18010adeee
Brian O'Hare <bjohare@gmail.com>
2016-02-04 10:30:18 +0000

    Switching to swig html templates. Introducing turf.js server side.

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,44 @@
-var leaflet = require('leaflet');
+var L = require('leaflet');
 var $ = require('jquery');
 
 var app = {};
 
-app.log = (function(){
+app.map = (function(){
 
     return {
         init: function(){
             console.log('running...');
+            initMap();
         }
+    }
+
+    function initMap(){
+        map = new L.Map('map');
+        // create the tile layer with correct attribution
+        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 17, attribution: osmAttrib});
+
+        // start the map in cloughjordan
+        map.setView(new L.LatLng(52.9403, -8.0356),12);
+        map.addLayer(osm);
+        var townlands = new L.geoJson();
+        townlands.addTo(map);
+
+        $.ajax({
+        dataType: "json",
+        url: "/townlands.json",
+        success: function(data) {
+            $(data.features).each(function(key, data) {
+                console.log(data.properties.area);
+                townlands.addData(data);
+            });
+        }
+        }).error(function() {});
     }
 
 }());
 
 $(document).ready(function() {
-        app.log.init();
+        app.map.init();
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
+var pg = require('pg');
+var turf = require('turf');
 var router = express.Router();
+
+var connStr = "postgres://gis:@/osm_data_analysis";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'OSM Data Analysis Prototype'
+    });
 });
+
+router.get('/map', function(req, res) {
+    res.render('map', {
+        title: 'OSM Data Analysis Prototype',
+    });
+});
+
+router.get('/townlands.json', function(req, res) {
+    var client = new pg.Client(connStr);
+    client.connect();
+    var query = client.query("SELECT id, name, ST_AsGeoJSON(ST_Transform(the_geom, 4326)) as geometry FROM cloughjordan_townlands");
+    var features = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    query.on("row", function(row, result) {
+        var feat = {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {}
+        }
+        var geom = row.geometry;
+        feat.geometry = JSON.parse(geom);
+        var properties = {};
+        properties.id = row.id;
+        properties.name = row.name;
+        properties.area = turf.area(feat.geometry);
+        feat.properties = properties;
+        result.addRow(feat);
+    });
+
+    query.on("end", function(result) {
+        features.features = result.rows;
+        res.type('application/json');
+        res.send(features);
+    });
+});
+
 
 module.exports = router;
